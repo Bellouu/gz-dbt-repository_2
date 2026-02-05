557f5d9c36819518397f8e0c2f4fa9219bebcc06
marcel.benja@gmail.com <marcel.benja@gmail.com>
2026-02-05 16:16:44 +0000

    correction test int_compaign_day

--- models/intermediate/int_campaigns_day.sql
+++ models/intermediate/int_campaigns_day.sql
@@ -1,8 +1,8 @@
 SELECT
-    date_date AS date,
-    ROUND(SUM(ads_cost), 2) AS ads_cost,
-    SUM(impression) AS impressions,
-    SUM(click) AS click
+  date_date,
+  ROUND(SUM(ads_cost), 2) AS ads_cost,
+  SUM(impression) AS impressions,
+  SUM(click) AS clicks
 FROM {{ ref('int_campaigns') }}
+WHERE date_date IS NOT NULL
 GROUP BY date_date
-ORDER BY date_date DESC
