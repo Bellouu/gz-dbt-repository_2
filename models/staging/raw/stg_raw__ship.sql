de0e5a0034f68cd174870da4fbb191d29c2144b7
marcel.benja@gmail.com <marcel.benja@gmail.com>
2026-02-05 11:23:30 +0000

    commit modif cast

--- models/staging/raw/stg_raw__ship.sql
+++ models/staging/raw/stg_raw__ship.sql
@@ -1,22 +1,17 @@
 with 
 
 source as (
-
     select * from {{ source('raw', 'ship') }}
-
 ),
 
 renamed as (
-
     select
         orders_id,
         shipping_fee,
-        shipping_fee_1,
-        logcost,
-        ship_cost
+        logcost AS log_cost,
+        CAST(ship_cost AS FLOAT64) AS ship_cost
 
     from source
-
 )
 
 select * from renamed
